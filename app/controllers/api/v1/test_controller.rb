e3bb0c74e6aad6efa8bc101ff6179f70821b0ade
Leandro <leandro83g@icloud.com>
2021-07-08 22:39:37 +0900

    CORS setting

---
+++ app/controllers/api/v1/test_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::TestController < ApplicationController
+    def index
+
+        render json: { message: "hello world!"}
+        
+    end
+end
---
+++ test/controllers/api/v1/test_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::TestControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
