9773d7b32674e833df523ef078964607d1d7bc40
Leandro <leandro83g@icloud.com>
2021-07-17 10:10:26 +0900

    seedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |i|
+    User.create!(
+        email: "user#{i.to_s}@exam.com",
+        password: 'aaaaaa',
+        password_confirmation: 'aaaaaa',
+        name: "user#{i.to_s}"
+    )
+end
+
+
+10.times do |i|
+    DailyRanking.create!(
+        user_id: i
+    )
+end
+
