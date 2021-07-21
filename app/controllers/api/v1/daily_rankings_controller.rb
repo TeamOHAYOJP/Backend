33edce9ad9f884f1ed6e899327f76dca64748fa1
Leandro <leandro83g@icloud.com>
2021-07-21 18:42:02 +0900

    間違えて違うところ編集しちゃったけどひとまずコミット

--- app/controllers/api/v1/daily_rankings_controller.rb
+++ app/controllers/api/v1/daily_rankings_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::DailyRankingsController < ApplicationController
   end
 
   def create
-    @daily_ranking = DailyRanking.new(params[:user_id]) 
-    @ranking = Ranking.new(params[:user_id])
+    @daily_ranking = DailyRanking.new(dailiy_rankings_params) 
+    @ranking = Ranking.new(dailiy_rankings_params)
 
     if @daily_ranking.save && @ranking.save
       render json: { status: 200, message: "保存し成功しました", daily_ranking: @daily_ranking , ranking: @ranking}
@@ -38,7 +38,7 @@ class Api::V1::DailyRankingsController < ApplicationController
   end
   private
 
-  # def dailiy_rankings_params
-  #   params.permit(:user_id)
-  # end
+  def dailiy_rankings_params
+    params.permit(:user_id)
+  end
 end
--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,2 +1,3 @@
 class Ranking < ApplicationRecord
+    belongs_to :user
 end
