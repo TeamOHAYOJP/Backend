4ee4c157d53d202c6bb7f754c70faa6fb1525411
Leandro <leandro83g@icloud.com>
2021-07-22 11:55:56 +0900

    is_ranked_inのレスポンスを少し追加

--- app/controllers/api/v1/daily_rankings_controller.rb
+++ app/controllers/api/v1/daily_rankings_controller.rb
@@ -30,8 +30,10 @@ class Api::V1::DailyRankingsController < ApplicationController
       return
     end
 
-    if DailyRanking.where(user_id: current_api_v1_user.id).present?
-      render json: { status: 200, is_ranked_in: true }
+
+    @rankings = DailyRanking.where(user_id: current_api_v1_user.id)
+    if @rankings.present?
+      render json: { status: 200, is_ranked_in: true, ranking: @rankings[-1] }
     else
       render json: { status: 200, is_ranked_in: false }
     end
